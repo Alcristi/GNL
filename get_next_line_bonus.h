49f866ad8bf94bc8a8d3cea1e37cf0754eb0d226
Allan Cristian <alcristi.sto@gmail.com>
2021-09-15 07:56:40 -0300

    Projeto Finalizado

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,8 +1,21 @@
-#ifndef GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcristi <alcristi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 07:54:05 by alcristi          #+#    #+#             */
+/*   Updated: 2021/09/15 07:55:15 by alcristi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define	GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include<stdlib.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
